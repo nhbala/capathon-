ef58f13beec95e7c7a7b6ab1354a5b1d9ef2e0fc
nhbala <nb378@cornell.edu>
2019-06-06 00:39:51 -0400

    aioshfosga

--- landingpage.py
+++ landingpage.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+from processing_vector import find_charities
+import json
 app = Flask(__name__)
 
 @app.route('/')
@@ -7,7 +9,9 @@ def start():
 
 @app.route('/give_back')
 def give_back():
-    return render_template('extended.html')
+    json_object = json.load(open("test_user_vector.json"))
+    returned_charities = find_charities(json_object)
+    return render_template('extended.html', charity_list=returned_charities)
 
 
 if __name__ == '__main__':
